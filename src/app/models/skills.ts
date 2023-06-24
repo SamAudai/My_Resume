e3992f159be64a2141763fa47950f1870160a69a
audai <audai123@yahoo.com>
2023-06-25 00:36:10 +0300

    first commit

--- src/app/models/skills.ts
+++ src/app/models/skills.ts
@@ -9,73 +9,73 @@ export const SKILLS: skills[] = [
     {
       id: 0,
       name: " ",
-      image: "../assets/img/html.svg",
+      image: "assets/img/html.svg",
       value: 0      
     },
     {
       id: 1,
       name: " ",
-      image: "../assets/img/css.svg",
+      image: "assets/img/css.svg",
       value: 0      
     },
     {
       id: 2,
       name: " ",
-      image: "../assets/img/javascript.svg",
+      image: "assets/img/javascript.svg",
       value: 0      
     },
     {
       id: 3,
       name: " ",
-      image: "../assets/img/sass.svg",
+      image: "assets/img/sass.svg",
       value: 0      
     },
     {
       id: 4,
       name: " ",
-      image: "../assets/img/angular.svg",
+      image: "assets/img/angular.svg",
       value: 0      
     },
     {
       id: 5,
       name: " ",
-      image: "../assets/img/node.svg",
+      image: "assets/img/node.svg",
       value: 0      
     },
     {
       id: 6,
       name: " ",
-      image: "../assets/img/dot-net-core-7.svg",
+      image: "assets/img/dot-net-core-7.svg",
       value: 0      
     },
     {
       id: 7,
       name: " ",
-      image: "../assets/img/c_sharp.svg",
+      image: "assets/img/c_sharp.svg",
       value: 0      
     },
     {
       id: 8,
       name: " ",
-      image: "../assets/img/java.svg",
+      image: "assets/img/java.svg",
       value: 0      
     },
     {
       id: 9,
       name: " ",
-      image: "../assets/img/c.svg",
+      image: "assets/img/c.svg",
       value: 0      
     },
     {
       id: 10,
       name: " ",
-      image: "../assets/img/microsoft-sql-server.svg",
+      image: "assets/img/microsoft-sql-server.svg",
       value: 0      
     },
     {
       id: 11,
       name: " ",
-      image: "../assets/img/mongodb.svg",
+      image: "assets/img/mongodb.svg",
       value: 0      
     }
   ]
